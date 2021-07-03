6f261851b303cce2f7834e1ebc84bc67ec4436c1
Thomas A. Alvarado Davalos <talvdav@mailbox.org>
2021-07-03 20:13:37 +0200

    using straight.el now

--- bootstrap.el
+++ bootstrap.el
@@ -13,23 +13,39 @@
 ;; Initialize package sources
 (require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+;(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+;                         ("org" . "https://orgmode.org/elpa/")
+;                         ("elpa" . "https://elpa.gnu.org/packages/")))
+;
+;(package-initialize)
+;(unless package-archive-contents
+;  (package-refresh-contents))
+;
+;;; Initialize use-package on non-Linux platforms
+;(unless (package-installed-p 'use-package)
+;  (package-install 'use-package))
+;
+;(require 'use-package)
+;(setq use-package-always-ensure t)
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+      (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
+(straight-use-package 'use-package)
 
-(require 'use-package)
-(setq use-package-always-ensure t)
+(setq straight-use-package-by-default t)
 
-(use-package org
-  :pin org)
+(use-package org)
 
 (use-package all-the-icons)
 (all-the-icons-install-fonts t)

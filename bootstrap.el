157b8e2ca0745ec9366e052d51cad5df231d50a9
Thomas A. Alvarado Davalos <thomas.alvarado@hoechstadt.de>
2021-09-14 13:24:25 +0200

    cleaned up a bit

--- bootstrap.el
+++ bootstrap.el
@@ -2,34 +2,36 @@
 (setq user-emacs-directory  "~/.emacs.d/cache/")
 
 (defvar bootstrap-version)
-     (let ((bootstrap-file
-	   (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-	   (bootstrap-version 5))
-       (unless (file-exists-p bootstrap-file)
-	 (with-current-buffer
-	     (url-retrieve-synchronously
-	     "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	     'silent 'inhibit-cookies)
-	   (goto-char (point-max))
-	   (eval-print-last-sexp)))
-       (load bootstrap-file nil 'nomessage))
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-     (straight-use-package 'use-package)
+(straight-use-package 'use-package)
 
-     (setq straight-use-package-by-default t)
+(setq straight-use-package-by-default t)
 
 (use-package org)
+
 (use-package evil
   :config
   (evil-mode 1))
 
 (blink-cursor-mode 0)
+
 (use-package evil-escape
-   :straight t
-   :diminish
-   :init (setq-default evil-escape-key-sequence "jk")
-   :config (evil-escape-mode 1))
- 
+  :straight t
+  :diminish
+  :init (setq-default evil-escape-key-sequence "jk")
+  :config (evil-escape-mode 1))
+
 (defun efs/org-babel-tangle-config ()
   (when (string-equal (file-name-directory (buffer-file-name))
                       (expand-file-name *config-directory*))
@@ -37,7 +39,8 @@
     (let ((org-confirm-babel-evaluate nil))
       (org-babel-tangle))))
 
-(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
+(add-hook 'org-mode-hook
+          (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
 
 (with-eval-after-load 'org
   (org-babel-do-load-languages
@@ -54,9 +57,6 @@
   (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
   (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp")))
 
-(setq *config-directory* user-emacs-directory)
-(setq user-emacs-directory  "~/.emacs.d/cache/")
-
 (use-package no-littering)
 
 (setq aut-save-file-name-transforms
@@ -65,6 +65,6 @@
 (org-babel-tangle-file "~/.emacs.d/Init.org")
 
 (if (file-exists-p "~/.emacs")
-	   (delete-file "~/.emacs"))
+    (delete-file "~/.emacs"))
 
 (load-file "~/.emacs.d/init.el")

54586980b17a5d624d80bd4eace2058930fc5a84
Thomas A. Alvarado Davalos <thomas.alvarado@mailbox.org>
2022-11-22 16:02:23 +0100

    added kotlin-mode for gradle.kts files

--- conf/java.el
+++ conf/java.el
@@ -1,4 +1,7 @@
 (use-package javap-mode)
+(use-package kotlin-mode) ;; for gradle kotlin script files
 
 (use-package lsp-java
   :hook (java-mode . lsp))
+
+(add-to-list 'auto-mode-alist '("\\.kts\\'" . kotlin-mode))

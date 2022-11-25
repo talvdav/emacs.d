f994216795d19cbbb33217233d14d26c58bd8014
Thomas A. Alvarado Davalos <thomas.alvarado@mailbox.org>
2022-11-25 09:55:28 +0100

    added scala-mode

--- conf/java.el
+++ conf/java.el
@@ -1,7 +1,10 @@
 (use-package javap-mode)
 (use-package kotlin-mode) ;; for gradle kotlin script files
+(use-package scala-mode)
 
 (use-package lsp-java
-  :hook (java-mode . lsp))
+  :hook (java-mode . lsp)
+  :hook (scala-mode . lsp)
+  :hook (kotlin-mode . lsp))
 
 (add-to-list 'auto-mode-alist '("\\.kts\\'" . kotlin-mode))

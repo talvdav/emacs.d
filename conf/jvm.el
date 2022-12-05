18a77a6bffdd57dab3a5e58de55ed6b6d8a39f52
Thomas A. Alvarado Davalos <thomas.alvarado@mailbox.org>
2022-12-05 14:34:42 +0100

    update

--- conf/jvm.el
+++ conf/jvm.el
@@ -4,10 +4,10 @@
 (use-package javap-mode)
 (use-package kotlin-mode) ;; for gradle kotlin script files
 (use-package scala-mode)
+(use-package groovy-mode)
 
 (use-package lsp-java
   :hook (java-mode . lsp)
-  :hook (scala-mode . lsp)
   :hook (kotlin-mode . lsp))
 
 (add-to-list 'auto-mode-alist '("\\.kts\\'" . kotlin-mode))
